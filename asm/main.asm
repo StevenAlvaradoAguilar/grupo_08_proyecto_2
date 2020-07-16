2bc70730030fcda76d2ef2cf0aa8b49063dc7b89
StevenAlvaradoAguilar <48374564+StevenAlvaradoAguilar@users.noreply.github.com>
2020-07-16 12:57:01 -0600

    Entrega del avance 1
    Se crea el proyecto en visual con algunos de los archivos, se hacen los archivos de Octave y se agregan los archivos de vs en la carpeta asm.

---
+++ asm/main.asm
@@ -0,0 +1,24 @@
+; Descripcion del Programa:
+; Autor:
+; Dia:
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+
+.data
+; declarar variables aqui
+
+.code
+
+main PROC
+ ; Escribir codigo aqui 
+
+ INVOKE ExitProcess,0
+
+main ENDP
+
+; (insertar procesos adicionales aqui)
+
+END main
