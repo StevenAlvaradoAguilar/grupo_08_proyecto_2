9159f9cf3d1d0892a3dc04a853368408cab6a99f
StevenAlvaradoAguilar <48374564+StevenAlvaradoAguilar@users.noreply.github.com>
2020-07-31 22:07:16 -0600

    Arreglo del proyecto
    Agregamos arreglo del prototipo del proyecto 2 grupo 08

--- proto/ac_atan2.m
+++ proto/ac_atan2.m
@@ -19,32 +19,36 @@
 
 function [theta, octant] = ac_atan2(real_part, imag_part)
   
-  real_mult_imag = real_part * imag_part;
-  num_pot_15 = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2; % DK: Esto es 2^15
-  pi_round = round(3.141592654 * num_pot_15);                             % DK: Poner valor entero exacto, no calculado
-  pi_n_round = -pi_round;
-  pi_half = pi_round / (2);
-  pi_n_half = -pi_half;
-  pi_quarts = pi_round / 4;                                               % DK: Poner valor entero exacto, no calculado
-  pi_n_quarts = -pi_quarts;
-  three_pi_quarts = (3 * pi_round) / 4;
-  three_n_pi_quarts = -three_pi_quarts;
+  REAL_MULT_IMAG = real_part * imag_part;
+  PI_ROUND = 102944;
+  PI_N_ROUND = -PI_ROUND;
+  PI_HALF = 51472;
+  PI_N_HALF = -PI_HALF;
+  PI_QUARTS = 25736;
+  PI_N_QUARTS = -PI_QUARTS;
+  THREE_PI_QUARTS = (3 * PI_ROUND) / 4;
+  THREE_N_PI_QUARTS = -THREE_PI_QUARTS;
   
-  x_pot_2 = real_part * real_part;
-  x_mov_2_right = bitshift(x_pot_2, -2);
-  x_mov_5_right = bitshift(x_pot_2, -5);
-  y_pot_2 = imag_part * imag_part;
-  y_mov_2_right = bitshift(y_pot_2,-2);
-  y_mov_5_right = bitshift(y_pot_2,-5);
+  ABSO_REAL_PART = abs(real_part);
+  ABSO_IMAG_PART = abs(imag_part);
+  X_POT_2 = real_part * real_part;
+  Y_POT_2 = imag_part * imag_part;
+  X_MOV_2_RIGHT = bitshift(X_POT_2, -2);
+  X_MOV_5_RIGHT = bitshift(X_POT_2, -5);
+  Y_MOV_2_RIGHT = bitshift(Y_POT_2,-2);
+  Y_MOV_5_RIGHT = bitshift(Y_POT_2,-5);
+  SUM_X_Y = X_POT_2 + Y_MOV_2_RIGHT + Y_MOV_5_RIGHT;
+  SUM_Y_X = Y_POT_2 + X_MOV_2_RIGHT + X_MOV_5_RIGHT;
   
-    %Indefined function
-    if real_part == 0 && imag_part == 0
-      
-      octant = 0;
-      theta = 0;
-      return;
+  %Indefined function
+  if real_part == 0 && imag_part == 0
     
-    endif
+    octant = 0;
+    theta = 0;
+    return;
+    
+  endif
+   
   %The angle 0 and 360
   if real_part > 0 && imag_part == 0
     octant = 0;
@@ -57,7 +61,7 @@ function [theta, octant] = ac_atan2(real_part, imag_part)
   if real_part == imag_part && real_part > 0
       
       octant = 0;
-      theta = round(pi_quarts);
+      theta = PI_QUARTS;
       return;
     
    endif
@@ -66,16 +70,16 @@ function [theta, octant] = ac_atan2(real_part, imag_part)
   if real_part == 0 && imag_part > 0
       
       octant = 0;
-      theta = round(pi_half);
+      theta = PI_HALF;
       return;
     
    endif
    
    %The angle 135°
-  if abs(real_part) == imag_part > 0
+  if ABSO_REAL_PART == imag_part > 0
       
       octant = 0;
-      theta = round(three_pi_quarts);
+      theta = THREE_PI_QUARTS;
       return;
     
    endif
@@ -84,16 +88,16 @@ function [theta, octant] = ac_atan2(real_part, imag_part)
   if real_part < 0 && imag_part == 0
       
       octant = 0;
-      theta = round(pi_round);
+      theta = PI_ROUND;
       return;
     
    endif
    
    %The angle 225°
-  if abs(real_part) == abs(imag_part) && real_part < 0
+  if ABSO_REAL_PART == ABSO_IMAG_PART && real_part < 0
       
       octant = 0;
-      theta = round(three_n_pi_quarts);
+      theta = THREE_N_PI_QUARTS;
       return;
     
    endif
@@ -102,16 +106,16 @@ function [theta, octant] = ac_atan2(real_part, imag_part)
   if real_part == 0 && imag_part < 0
       
       octant = 0;
-      theta = round(pi_n_half);
+      theta = PI_N_HALF;
       return;
     
    endif
    
    %The angle 315°
-  if real_part == abs(imag_part)
+  if real_part == ABSO_IMAG_PART
       
       octant = 0;
-      theta = round(pi_n_quarts);
+      theta = PI_N_QUARTS;
       return;
     
    endif
@@ -123,9 +127,8 @@ function [theta, octant] = ac_atan2(real_part, imag_part)
      if real_part > imag_part 
       
           octant = 1;
-          theta1 = x_pot_2 + y_mov_2_right + y_mov_5_right;  % DK: Hacer esto sólo una vez arriba y poner en una variable
-          theta1 = bitshift(theta1, -15);
-          theta1 = real_mult_imag / (theta1);
+          theta1 = bitshift(SUM_X_Y, -15);
+          theta1 = REAL_MULT_IMAG / (theta1);
           theta = round(theta1);
           return;
           
@@ -135,10 +138,9 @@ function [theta, octant] = ac_atan2(real_part, imag_part)
      if real_part <= imag_part
        
        octant = 2;
-       thet1 = y_pot_2 + x_mov_2_right + x_mov_5_right;       % DK: Hacer esto sólo una vez arriba y poner en una variable
-       theta1 = bitshift(thet1, -15);
-       theta1 = real_mult_imag / (theta1);
-       theta = round(pi_half - theta1); %DK: Usar mayusculas para constantes
+       theta1 = bitshift(SUM_Y_X, -15);
+       theta1 = REAL_MULT_IMAG / (theta1);
+       theta = round(PI_HALF - theta1);
        return;
         
      endif
@@ -149,25 +151,23 @@ function [theta, octant] = ac_atan2(real_part, imag_part)
    if real_part < 0 && imag_part >= 0
      
      %The octan 3
-     if abs(real_part) <= imag_part 
+     if ABSO_REAL_PART <= imag_part 
        
        octant = 3;
-       thet1 = y_pot_2 + x_mov_2_right + x_mov_5_right;
-       theta1 = bitshift(thet1, -15);
-       theta1 = real_mult_imag / (theta1);
-       theta = round(pi_half - theta1);
+       theta1 = bitshift(SUM_Y_X, -15);
+       theta1 = REAL_MULT_IMAG / (theta1);
+       theta = round(PI_HALF - theta1);
           return;
           
      endif
      
      %The octan 4
-     if abs(real_part) > imag_part % DK: asignar abs(real_part) a una variable arriba
+     if ABSO_REAL_PART > imag_part 
        
-       octant = 4;
-       theta1 = x_pot_2 + y_mov_2_right + y_mov_5_right;  
-       theta1 = bitshift(theta1, -15);
-       theta1 = real_mult_imag / (theta1);
-       theta = round(pi_round + theta1);
+       octant = 4;  
+       theta1 = bitshift(SUM_X_Y, -15);
+       theta1 = REAL_MULT_IMAG / (theta1);
+       theta = round(PI_ROUND + theta1);
        return;
         
      endif
@@ -178,25 +178,23 @@ function [theta, octant] = ac_atan2(real_part, imag_part)
    if real_part < 0 && imag_part < 0
      
      %The octan 5
-     if abs(real_part) > abs(imag_part)
+     if ABSO_REAL_PART > ABSO_IMAG_PART
       
       octant = 5;
-      theta1 = x_pot_2 + y_mov_2_right + y_mov_5_right;  
-      theta1 = bitshift(theta1, -15);
-      theta1 = real_mult_imag / (theta1);
-      theta = round(pi_n_round + theta1);
+      theta1 = bitshift(SUM_X_Y, -15);
+      theta1 = REAL_MULT_IMAG / (theta1);
+      theta = round(PI_N_ROUND + theta1);
       return;
         
      endif
      
      %The octan 6
-     if abs(real_part) <= abs(imag_part) 
+     if ABSO_REAL_PART <= ABSO_IMAG_PART 
       
       octant = 6;
-      theta1 = y_pot_2 + x_mov_2_right + x_mov_5_right;  
-      theta1 = bitshift(theta1, -15);
-      theta1 = real_mult_imag / (theta1);
-      theta = round(pi_n_half - theta1);
+      theta1 = bitshift(SUM_Y_X, -15);
+      theta1 = REAL_MULT_IMAG / (theta1);
+      theta = round(PI_N_HALF - theta1);
       return;   
           
      endif
@@ -207,24 +205,22 @@ function [theta, octant] = ac_atan2(real_part, imag_part)
    if real_part >= 0 && imag_part < 0
      
      %The octan 7
-     if real_part <= abs(imag_part)
+     if real_part <= ABSO_IMAG_PART
        
        octant = 7;
-       theta1 = y_pot_2 + x_mov_2_right + x_mov_5_right;  
-       theta1 = bitshift(theta1, -15);
-       theta1 = real_mult_imag / (theta1);
-       theta = round(pi_n_half - theta1);
+       theta1 = bitshift(SUM_Y_X, -15);
+       theta1 = REAL_MULT_IMAG / (theta1);
+       theta = round(PI_N_HALF - theta1);
        return;
         
      endif
      
      %The octan 8
-     if real_part > abs(imag_part) 
+     if real_part > ABSO_IMAG_PART 
        
        octant = 8;
-       theta1 = x_pot_2 + y_mov_2_right + y_mov_5_right;  
-       theta1 = bitshift(theta1, -15);
-       theta1 = real_mult_imag / (theta1);
+       theta1 = bitshift(SUM_X_Y, -15);
+       theta1 = REAL_MULT_IMAG / (theta1);
        theta = round(theta1);
        return;
           
