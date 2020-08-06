1af671cc83c5e15734cb81a084c2e72c7482b437
StevenAlvaradoAguilar <48374564+StevenAlvaradoAguilar@users.noreply.github.com>
2020-08-05 20:47:29 -0600

    2 Avance
    Se hace el segundo avance del proyecto 2 del grupo 08.

--- vs/grupo_08_proyecto_2/grupo_08_proyecto_2/ac_atan2.asm
+++ vs/grupo_08_proyecto_2/grupo_08_proyecto_2/ac_atan2.asm
@@ -11,13 +11,13 @@ ExitProcess PROTO, dwExitCode:DWORD
 ; declarar variables aqui
 REAL_MULT_IMAG  DWORD 0
 PI_ROUND DWORD 102944
-PI_N_ROUND DWORD 0 ; negativo (tal vez ocupe conversion)
+PI_N_ROUND DWORD 0 ; negativo 
 PI_HALF DWORD 51472
-PI_N_HALF DWORD 0 ; negativo (tal vez ocupe conversion)
+PI_N_HALF DWORD 0 ; negativo 
 PI_QUARTS DWORD 25736
-PI_N_QUARTS DWORD 0 ; negativo (tal vez ocupe conversion)
-THREE_PI_QUARTS DWORD 0
-THREE_N_PI_QUARTS DWORD 0 ; negativo (tal vez ocupe conversion)
+PI_N_QUARTS DWORD 0 ; negativo 
+THREE_PI_QUARTS DWORD 77208
+THREE_N_PI_QUARTS DWORD 0 ; negativo 
   
 ABSO_REAL_PART DWORD 0
 ABSO_IMAG_PART DWORD 0
@@ -40,9 +40,170 @@ THETA_Y DWORD 0
 
 main PROC
  ; Escribir codigo aqui 
- (3 * PI_ROUND) / 4
+ ac_atan2 PROC real_part:DWORD, imag_part:DWORD
+	
+	;Function real_mult_imag
+	MOV EAX, real_part
+	MOV EBX, imag_part
+	MUL EAX, EBX
+	MOV REAL_MULT_IMAG, EAX
 
- INVOKE ExitProcess,0
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+	;Function neg pi_round
+	mov  EAX, PI_ROUND
+	NEG EAX
+	MOV PI_N_ROUND, EAX
+
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+	;Function neg pi_half
+	mov  EAX, PI_HALF
+	NEG EAX
+	MOV PI_N_HALF, EAX
+
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+	;Function neg pi_quarts
+	mov  EAX, PI_QUARTS
+	NEG EAX
+	MOV PI_N_QUARTS, EAX
+
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+	;Function neg tree_pi_quarts
+	mov  EAX, THREE_PI_QUARTS
+	NEG EAX
+	MOV THREE_N_PI_QUARTS, EAX
+
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+	;Function absolute real_part
+	; abs(eax), with no branches.
+	; intel syntax (dest, src)
+	MOV EAX, real_part
+	mov EBX, EAX ;store eax in ebx
+	neg EAX
+	cmovl EAX, real_part ;if eax is now negative, restore its saved value
+	MOV ABSO_REAL_PART, EAX
+
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+	;Function absolute imag_part
+	; abs(eax), with no branches.
+	; intel syntax (dest, src)
+	MOV EAX, imag_part
+	mov EBX, EAX ;store eax in ebx
+	neg EAX
+	cmovl EAX, imag_part ;if eax is now negative, restore its saved value
+	MOV ABSO_IMAG_PART, EAX
+
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+	;Function real_part mult real_part
+	MOV EAX, real_part
+	MOV EBX, real_part
+	MUL EAX, EBX
+	MOV X_POT_2, EAX
+
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+	;Function real_part mult real_part
+	MOV EAX, imag_part
+	MOV EBX, imag_part
+	MUL EAX, EBX
+	MOV Y_POT_2, EAX
+
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+	;Function real_part mov 2 right
+	MOV ECX, X_POT_2
+	SHR ECX, 2
+	MOV X_MOV_2_RIGHT, ECX
+
+	XOR ECX, ECX
+
+	;Function real_part mov 5 right
+	MOV ECX, X_POT_2
+	SHR ECX, 5
+	MOV X_MOV_5_RIGHT, ECX
+
+	XOR ECX, ECX
+
+	;Function imag_part mov 2 right
+	MOV ECX, Y_POT_2
+	SHR ECX, 2
+	MOV Y_MOV_2_RIGHT, ECX
+
+	;Function imag_part mov 5 right
+	MOV ECX, Y_POT_2
+	SHR ECX, 5
+	MOV Y_MOV_5_RIGHT, ECX
+
+	XOR ECX, ECX
+
+	;Function sum x_pot_2 + y_mov_2_right + y_mov_5_right
+	MOV EAX, X_POT_2
+	MOV EBX, Y_MOV_2_RIGHT
+	ADD EAX, EBX
+	XOR EBX, EBX
+	MOV EBX, Y_MOV_5_RIGHT
+	ADD EAX, EBX
+	MOV SUM_X_Y, EAX
+
+
+	;Function sum_x_y mov 15 right
+	MOV ECX, SUM_X_Y
+	SHR ECX, 15
+	MOV MOV_X_15_RIGHT, ECX
+
+	XOR ECX, ECX
+
+	;Function real_mult_imag div mov_x_rigth
+	MOV EAX, REAL_MULT_IMAG
+	MOV EBX, MOV_X_15_RIGHT
+	DIV EAX, EBX
+	MOV THETA_X, EAX
+
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+	;Function sum y_pot_2 + x_mov_2_right + x_mov_5_right
+	MOV EAX, Y_POT_2
+	MOV EBX, X_MOV_2_RIGHT
+	ADD EAX, EBX
+	XOR EBX, EBX
+	MOV EBX, X_MOV_5_RIGHT
+	ADD EAX, EBX
+	MOV SUM_Y_X, EAX
+
+
+	;Function sum_x_y mov 15 right
+	MOV ECX, SUM_Y_X
+	SHR ECX, 15
+	MOV MOV_Y_15_RIGHT, ECX
+
+	XOR ECX, ECX
+
+	;Function real_mult_imag div mov_x_rigth
+	MOV EAX, REAL_MULT_IMAG
+	MOV EBX, MOV_Y_15_RIGHT
+	DIV EAX, EBX
+	MOV THETA_Y, EAX
+
+	XOR EAX, EAX
+	XOR EBX, EBX
+
+ INVOKE ExitProcess, 0
 
 main ENDP
 
--- vs/grupo_08_proyecto_2/grupo_08_proyecto_2/main.asm
+++ vs/grupo_08_proyecto_2/grupo_08_proyecto_2/main.asm
@@ -5,16 +5,34 @@
 .386
 .model flat,stdcall
 .stack 4096
+
 ExitProcess PROTO, dwExitCode:DWORD
+
+;We include the libraries links to Irvine so we can call the Irvine procedures
+include c:\Irvine\Irvine32.inc
+includelib c:\Irvine\Irvine32.lib
+includelib c:\Irvine\Kernel32.lib
+includelib c:\Irvine\user32.lib
+include io_functions.inc ;We include an .inc file that declares a prototype of the function
+include ac_atan2.inc ;We include an .inc file that declares a prototype of the function
 
 .data
 ; declarar variables aqui
- 
+real_part DWORD 0
+imag_part DWORD 0
 
 .code
 
 main PROC
- ; Escribir codigo aqui 
+
+	;Calls the class RequestIntegers and gives it the data
+	INVOKE io_functions
+
+	MOV real_part, ebx	;
+	MOV	imag_part, eax		;
+	popad
+	;Calls the class uint8_mult and gives it the data,
+	INVOKE ac_atan2, real_part, imag_part 
 
  INVOKE ExitProcess,0
 
