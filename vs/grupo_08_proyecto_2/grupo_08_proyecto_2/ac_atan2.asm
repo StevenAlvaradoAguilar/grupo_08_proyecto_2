2eb6a20a6e919f7e1759229906b05575a7d5ce31
StevenAlvaradoAguilar <48374564+StevenAlvaradoAguilar@users.noreply.github.com>
2020-08-11 13:10:56 -0600

    Avance 4
    Subimos avance 4 con las funciones de los octantes y con un borrador de lectura y escritura de archivos.

--- vs/grupo_08_proyecto_2/grupo_08_proyecto_2/ac_atan2.asm
+++ vs/grupo_08_proyecto_2/grupo_08_proyecto_2/ac_atan2.asm
@@ -7,52 +7,83 @@
 .stack 4096
 ExitProcess PROTO, dwExitCode:DWORD
 
+;We include the libraries links to Irvine so we can call the Irvine procedures
+include c:\Irvine\Irvine32.inc
+includelib c:\Irvine\Irvine32.lib
+includelib c:\Irvine\Kernel32.lib
+includelib c:\Irvine\user32.lib
+include ac_atan2.inc            ;We include an .inc file that declares a prototype of the function
+
 .data
-; declarar variables aqui
-REAL_MULT_IMAG  DWORD 0
-PI_ROUND DWORD 102944
-PI_N_ROUND DWORD 0 ; negativo 
-PI_HALF DWORD 51472
-PI_N_HALF DWORD 0 ; negativo 
-PI_QUARTS DWORD 25736
-PI_N_QUARTS DWORD 0 ; negativo 
-THREE_PI_QUARTS DWORD 77208
-THREE_N_PI_QUARTS DWORD 0 ; negativo 
+; declarar constantes aqui
+REAL_MULT_IMAG    WORD 0
+PI_ROUND          DWORD 0
+PI_N_ROUND        DWORD 0          ; negativo 
+PI_HALF           WORD 0
+PI_N_HALF         WORD 0          ; negativo 
+PI_QUARTS         WORD 0
+PI_N_QUARTS       WORD 0          ; negativo 
+THREE_PI_QUARTS   DWORD 0
+THREE_N_PI_QUARTS DWORD 0          ; negativo 
+
+; declarar variantes aqui  
+abso_real_part    WORD 0
+abso_imag_part    WORD 0
+x_pot_2           WORD 0
+y_pot_2           WORD 0
+x_mov_2_right     WORD 0
+x_mov_5_right     WORD 0
+y_mov_2_right     WORD 0
+y_mov_5_right     WORD 0
   
-ABSO_REAL_PART DWORD 0
-ABSO_IMAG_PART DWORD 0
-X_POT_2 DWORD 0
-Y_POT_2 DWORD 0
-X_MOV_2_RIGHT DWORD 0
-X_MOV_5_RIGHT DWORD 0
-Y_MOV_2_RIGHT DWORD 0
-Y_MOV_5_RIGHT DWORD 0
+sum_x_y           WORD 0 
+mov_x_15_right    WORD 0  
+theta_x           WORD 0 
   
-SUM_X_Y DWORD 0 
-MOV_X_15_RIGHT DWORD 0  
-THETA_X DWORD 0 
-  
-SUM_Y_X DWORD 0
-MOV_Y_15_RIGHT DWORD 0
-THETA_Y DWORD 0
+sum_y_x           WORD 0
+mov_y_15_right    WORD 0
+theta_y           WORD 0
 
 .code
 
-main PROC
  ; Escribir codigo aqui 
- ac_atan2 PROC real_part:DWORD, imag_part:DWORD
+ ac_atan2 PROC real_part:WORD, imag_part:WORD, Octant:BYTE, Theta:DWORD
+
+	;Constant PI_ROUND
+	MOV EAX, 102944
+	MOV PI_ROUND, EAX
+
+	XOR EAX, EAX
+
+	;Constant PI_HALF
+	MOV AX, 51472
+	MOV PI_HALF, AX
+
+	XOR EAX, EAX
+
+	;Constant PI_QUARTS
+	MOV AX, 51472
+	MOV PI_QUARTS, AX
+
+	XOR EAX, EAX
+
+	;Constant THREE_PI_QUARTS
+	MOV EAX, 77208
+	MOV THREE_PI_QUARTS, EAX
+
+	XOR EAX, EAX
 	
 	;Function real_mult_imag
-	MOV EAX, real_part
-	MOV EBX, imag_part
-	MUL EAX, EBX
-	MOV REAL_MULT_IMAG, EAX
+	MOV AX, real_part
+	MOV BX, imag_part
+	MUL BX
+	MOV REAL_MULT_IMAG, BX
 
 	XOR EAX, EAX
 	XOR EBX, EBX
 
 	;Function neg pi_round
-	mov  EAX, PI_ROUND
+	mov EAX, PI_ROUND
 	NEG EAX
 	MOV PI_N_ROUND, EAX
 
@@ -60,23 +91,23 @@ main PROC
 	XOR EBX, EBX
 
 	;Function neg pi_half
-	mov  EAX, PI_HALF
-	NEG EAX
-	MOV PI_N_HALF, EAX
+	mov AX, PI_HALF
+	NEG AX
+	MOV PI_N_HALF, AX
 
 	XOR EAX, EAX
 	XOR EBX, EBX
 
 	;Function neg pi_quarts
-	mov  EAX, PI_QUARTS
-	NEG EAX
-	MOV PI_N_QUARTS, EAX
+	mov AX, PI_QUARTS
+	NEG AX
+	MOV PI_N_QUARTS, AX
 
 	XOR EAX, EAX
 	XOR EBX, EBX
 
 	;Function neg tree_pi_quarts
-	mov  EAX, THREE_PI_QUARTS
+	mov EAX, THREE_PI_QUARTS
 	NEG EAX
 	MOV THREE_N_PI_QUARTS, EAX
 
@@ -85,128 +116,584 @@ main PROC
 
 	;Function absolute real_part
 	; abs(eax), with no branches.
-	; intel syntax (dest, src)
-	MOV EAX, real_part
-	mov EBX, EAX ;store eax in ebx
-	neg EAX
-	cmovl EAX, real_part ;if eax is now negative, restore its saved value
-	MOV ABSO_REAL_PART, EAX
+	MOV AX, real_part
+	cdq
+	xor AX, DX
+	sub AX, DX
+	MOV abso_real_part, AX
 
 	XOR EAX, EAX
 	XOR EBX, EBX
 
 	;Function absolute imag_part
 	; abs(eax), with no branches.
-	; intel syntax (dest, src)
-	MOV EAX, imag_part
-	mov EBX, EAX ;store eax in ebx
-	neg EAX
-	cmovl EAX, imag_part ;if eax is now negative, restore its saved value
-	MOV ABSO_IMAG_PART, EAX
+	MOV AX, imag_part
+	cdq
+	xor AX, DX
+	sub AX, DX
+	MOV abso_imag_part, AX
 
 	XOR EAX, EAX
 	XOR EBX, EBX
 
 	;Function real_part mult real_part
-	MOV EAX, real_part
-	MOV EBX, real_part
-	MUL EAX, EBX
-	MOV X_POT_2, EAX
+	MOV AX, real_part
+	MOV BX, real_part
+	MUL BX
+	MOV x_pot_2, BX
 
 	XOR EAX, EAX
 	XOR EBX, EBX
 
 	;Function real_part mult real_part
-	MOV EAX, imag_part
-	MOV EBX, imag_part
-	MUL EAX, EBX
-	MOV Y_POT_2, EAX
+	MOV AX, imag_part
+	MOV BX, imag_part
+	MUL BX
+	MOV y_pot_2, BX
 
 	XOR EAX, EAX
 	XOR EBX, EBX
 
 	;Function real_part mov 2 right
-	MOV ECX, X_POT_2
-	SHR ECX, 2
-	MOV X_MOV_2_RIGHT, ECX
+	MOV CX, x_pot_2
+	SHR CX, 2
+	MOV x_mov_2_right, CX
 
 	XOR ECX, ECX
 
 	;Function real_part mov 5 right
-	MOV ECX, X_POT_2
-	SHR ECX, 5
-	MOV X_MOV_5_RIGHT, ECX
+	MOV CX, x_pot_2
+	SHR CX, 5
+	MOV x_mov_5_right, CX
 
 	XOR ECX, ECX
 
 	;Function imag_part mov 2 right
-	MOV ECX, Y_POT_2
-	SHR ECX, 2
-	MOV Y_MOV_2_RIGHT, ECX
+	MOV CX, y_pot_2
+	SHR CX, 2
+	MOV y_mov_2_right, CX
 
 	;Function imag_part mov 5 right
-	MOV ECX, Y_POT_2
-	SHR ECX, 5
-	MOV Y_MOV_5_RIGHT, ECX
+	MOV CX, y_pot_2
+	SHR CX, 5
+	MOV y_mov_5_right, CX
 
-	XOR ECX, ECX
+	XOR CX, CX
 
 	;Function sum x_pot_2 + y_mov_2_right + y_mov_5_right
-	MOV EAX, X_POT_2
-	MOV EBX, Y_MOV_2_RIGHT
-	ADD EAX, EBX
-	XOR EBX, EBX
-	MOV EBX, Y_MOV_5_RIGHT
-	ADD EAX, EBX
-	MOV SUM_X_Y, EAX
+	MOV AX, x_pot_2
+	MOV BX, y_mov_2_right
+	ADD AX, BX
+	XOR BX, BX
+	MOV BX, y_mov_5_right
+	ADD AX, BX
+	MOV sum_x_y, AX
 
 
 	;Function sum_x_y mov 15 right
-	MOV ECX, SUM_X_Y
-	SHR ECX, 15
-	MOV MOV_X_15_RIGHT, ECX
+	MOV CX, sum_x_y
+	SHR CX, 15
+	MOV mov_x_15_right, CX
 
 	XOR ECX, ECX
 
 	;Function real_mult_imag div mov_x_rigth
-	MOV EAX, REAL_MULT_IMAG
-	MOV EBX, MOV_X_15_RIGHT
-	DIV EAX, EBX
-	MOV THETA_X, EAX
+	MOV AX, REAL_MULT_IMAG
+	MOV BX, mov_x_15_right
+	DIV BX
+	MOV theta_x, BX
 
 	XOR EAX, EAX
 	XOR EBX, EBX
 
 	;Function sum y_pot_2 + x_mov_2_right + x_mov_5_right
-	MOV EAX, Y_POT_2
-	MOV EBX, X_MOV_2_RIGHT
-	ADD EAX, EBX
+	MOV AX, y_pot_2
+	MOV BX, x_mov_2_right
+	ADD AX, BX
 	XOR EBX, EBX
-	MOV EBX, X_MOV_5_RIGHT
-	ADD EAX, EBX
-	MOV SUM_Y_X, EAX
+	MOV BX, x_mov_5_right
+	ADD AX, BX
+	MOV sum_y_x, AX
 
 
 	;Function sum_x_y mov 15 right
-	MOV ECX, SUM_Y_X
-	SHR ECX, 15
-	MOV MOV_Y_15_RIGHT, ECX
+	MOV CX, sum_y_x
+	SHR CX, 15
+	MOV mov_y_15_right, CX
 
 	XOR ECX, ECX
 
 	;Function real_mult_imag div mov_x_rigth
-	MOV EAX, REAL_MULT_IMAG
-	MOV EBX, MOV_Y_15_RIGHT
-	DIV EAX, EBX
-	MOV THETA_Y, EAX
+	MOV AX, REAL_MULT_IMAG
+	MOV BX, mov_y_15_right
+	DIV BX
+	MOV theta_y, BX
 
 	XOR EAX, EAX
 	XOR EBX, EBX
 
+	; Special Angle (Indefined Function)
+	Undefined_angle:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		MOV BX, imag_part
+		ADD BX, AX
+		JZ Undefined_angle2
+		JNZ Angle_0_360
+
+	Undefined_angle2:
+		XOR EAX, EAX
+		XOR EBX, EBX
+		MOV AL, 0
+		MOV Octant, AL
+		MOV EBX, 0
+		MOV Theta, EBX
+		RET
+
+	;Special Angle 0 and 360 
+	;If real part is greater then zero continus check ,
+	;If imag part is equal to zero Angle found if not check other angle.
+	Angle_0_360:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		CMP AX, 0
+		JA Angle_0_360_2
+		JNA Angle_45
+		JAE Angle_45
+		JNAE Angle_45
+
+	Angle_0_360_2:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV BX, imag_part
+		ADD BX, 0
+		JZ Resultado
+		JNZ Angle_45
+
+	Resultado:
+		MOV AL, 0
+		MOV Octant, AL
+		MOV EBX, 0
+		MOV Theta, EBX
+		RET
+
+	;Angle 45
+	;If real part is greater then zero continue check, 
+	;then if real part and image part are the same angle found, 
+	;if not check other angle.
+	Angle_45:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		CMP AX, 0
+		JA Angle_45_2
+		JNA Angle_90
+		JAE Angle_90
+		JNAE Angle_90
+
+	Angle_45_2:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		MOV BX, imag_part
+		CMP AX, BX
+		JE Resultado_45
+		JNE Angle_90
+
+
+	Resultado_45:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 0
+		MOV Octant, AL
+		MOV BX, PI_QUARTS
+		MOV Theta, EBX
+		RET
+
+	; Angle 90
+	;If real part is zero continue check, 
+	;If imag part is greater then zero angle found, 
+	;if not check other angle.
+	Angle_90:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		ADD AX, 0
+		JZ Angle_90_2
+		JNZ Angle_135
+
+	Angle_90_2:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV BX, imag_part
+		CMP BX, 0
+		JA Resultado_90
+		JNA Angle_135
+		JAE Angle_135
+		JNAE Angle_135
+
+	Resultado_90:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 0
+		MOV Octant, AL
+		MOV BX, PI_HALF
+		MOV Theta, EBX
+		RET
+
+	;Angle 135
+	Angle_135:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV BX, imag_part
+		CMP BX, 0
+		JA Angle_135_2
+		JNA Angle_180
+		JAE Angle_180
+		JNAE Angle_180
+
+	Angle_135_2:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV BX, abso_real_part
+		CMP BX, AX
+		JE Resultado_135
+		JNE Angle_180    
+
+	Resultado_135:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 0
+		MOV Octant, AL
+		MOV EBX, THREE_PI_QUARTS
+		MOV Theta, EBX
+		RET
+
+	;Angle 180
+	Angle_180:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		CMP AX, 0
+		JNAE Angle_180_2
+		JNA Angle_225
+		JAE Angle_225
+		JA Angle_225
+
+	Angle_180_2:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV BX, imag_part
+		ADD BX, 0
+		JZ Resultado_180
+		JNZ Angle_225
+
+	Resultado_180:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 0
+		MOV Octant, AL
+		MOV EBX, PI_ROUND
+		MOV Theta, EBX
+		RET
+
+	;Angle 225
+	Angle_225:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		CMP AX, 0
+		JNAE Angle_225_2
+		JNA Angle_270
+		JAE Angle_270
+		JA Angle_270
+
+	Angle_225_2:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, abso_real_part 
+		MOV BX, abso_imag_part 
+		CMP AX, BX 
+		JE Resultado_225
+		JNE Angle_270
+
+	Resultado_225:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 0
+		MOV Octant, AL
+		MOV EBX, THREE_N_PI_QUARTS
+		MOV Theta, EBX
+		RET
+
+	;Angle 270
+	Angle_270:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV BX, real_part
+		ADD BX, 0
+		JZ Angle_270_2
+		JNZ Angle_315
+	
+	Angle_270_2:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, imag_part
+		CMP AX, 0
+		JNAE Resultado_270
+		JNA Angle_315
+		JAE Angle_315
+		JA Angle_315
+
+	Resultado_270:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 0
+		MOV Octant, AL
+		MOV BX, PI_N_HALF
+		MOV Theta, EBX
+
+	;Angle 315
+	Angle_315:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, abso_imag_part
+		MOV BX, real_part
+		Cmp AX, BX
+		JE Reultado_315
+		;JNE 
+
+	Reultado_315:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 0
+		MOV Octant, AL
+		MOV BX, PI_N_QUARTS
+		MOV Theta, EBX
+	
+	;
+	Octant_1:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		MOV BX, imag_part
+		CMP AX, BX
+		JA Result_Octant_1
+		JNA Octant_2
+		JAE Octant_2
+		JNAE Octant_2
+		
+
+	Result_Octant_1:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 1
+		MOV Octant, AL
+		MOV BX, theta_x
+		MOV Theta, EBX
+		RET
+
+	;
+	Octant_2:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		MOV BX, imag_part
+		CMP AX, BX
+		JB Octant_3
+		JBE Result_Octant_2
+		JNB Octant_3
+		JNBE Octant_3
+
+	Result_Octant_2:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 2
+		MOV Octant, AL
+		MOV BX, PI_HALF
+		MOV DX, theta_y
+		SUB BX, DX
+		MOV Theta, EBX
+		RET
+
+	;
+	Octant_3:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, abso_real_part
+		MOV BX, imag_part
+		CMP AX, BX
+		JB Octant_4
+		JBE Result_Octant_3
+		JNB Octant_4
+		JNBE Octant_4 
+
+	Result_Octant_3:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 3
+		MOV Octant, AL
+		MOV BX, PI_HALF
+		MOV DX, theta_y
+		SUB BX, DX
+		MOV Theta, EBX
+		RET
+
+	;
+	Octant_4:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, abso_real_part
+		MOV BX, imag_part
+		CMP AX, BX
+		JA Result_Octant_4 
+		JNA  Octant_5
+		JAE Octant_5
+		JNAE Octant_5
+
+	Result_Octant_4:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 4
+		MOV Octant, AL
+		MOV EBX, PI_ROUND
+		MOV DX, theta_x
+		ADD DX, BX
+		MOV Theta, EDX
+		RET
+
+	;
+	Octant_5:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, abso_real_part
+		MOV BX, abso_imag_part
+		CMP AX, BX
+		JA Result_Octant_5 
+		JNA  Octant_6
+		JAE Octant_6
+		JNAE Octant_6
+
+	Result_Octant_5:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 5
+		MOV Octant, AL
+		MOV EBX, PI_N_ROUND
+		MOV DX, theta_x
+		ADD DX, BX
+		MOV Theta, EDX
+		RET
+
+	;
+	Octant_6:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, abso_real_part
+		MOV BX, abso_imag_part
+		CMP AX, BX
+		JBE Result_Octant_6 
+		JB Octant_7
+		JNBE Octant_7
+		JNB Octant_7
+
+	Result_Octant_6:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 6
+		MOV Octant, AL
+		MOV BX, PI_N_HALF
+		MOV DX, theta_y
+		SUB BX, DX
+		MOV Theta, EBX
+		RET
+
+	;
+	Octant_7:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		MOV BX, abso_imag_part
+		CMP AX, BX
+		JAE Result_Octant_7 
+		JA Octant_8
+		JNA Octant_8
+		JNAE Octant_8
+
+	Result_Octant_7:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 7
+		MOV Octant, AL
+		MOV BX, PI_N_HALF
+		MOV DX, theta_y
+		SUB BX, DX
+		MOV Theta, EBX
+		RET
+
+	;
+	Octant_8:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AX, real_part
+		MOV BX, abso_imag_part
+		CMP AX, BX
+		JA Result_Octant_8
+
+	Result_Octant_8:
+		XOR EAX, EAX
+		XOR EBX, EBX
+
+		MOV AL, 8
+		MOV Octant, AL
+		MOV BX, theta_x
+		MOV Theta, EBX
+		RET
+
  INVOKE ExitProcess, 0
 
-main ENDP
+ac_atan2 ENDP
 
 ; (insertar procesos adicionales aqui)
 
-END main
+END
--- vs/grupo_08_proyecto_2/grupo_08_proyecto_2/main.asm
+++ vs/grupo_08_proyecto_2/grupo_08_proyecto_2/main.asm
@@ -9,17 +9,19 @@
 ExitProcess PROTO, dwExitCode:DWORD
 
 ;We include the libraries links to Irvine so we can call the Irvine procedures
-include c:\Irvine\Irvine32.inc
+include    c:\Irvine\Irvine32.inc
 includelib c:\Irvine\Irvine32.lib
 includelib c:\Irvine\Kernel32.lib
 includelib c:\Irvine\user32.lib
-include io_functions.inc ;We include an .inc file that declares a prototype of the function
-include ac_atan2.inc ;We include an .inc file that declares a prototype of the function
+include    io_functions.inc          ;We include an .inc file that declares a prototype of the function
+include    ac_atan2.inc              ;We include an .inc file that declares a prototype of the function
 
 .data
 ; declarar variables aqui
-real_part DWORD 0
-imag_part DWORD 0
+real_part WORD  8161
+imag_part WORD  714
+Octant    BYTE  0
+Theta     DWORD 0
 
 .code
 
@@ -28,11 +30,19 @@ main PROC
 	;Calls the class RequestIntegers and gives it the data
 	INVOKE io_functions
 
-	MOV real_part, ebx	;
-	MOV	imag_part, eax		;
+	MOV AX, real_part	;The value in x
+	MOV	BX, imag_part  ;The value in y
 	popad
+
 	;Calls the class uint8_mult and gives it the data,
-	INVOKE ac_atan2, real_part, imag_part 
+	INVOKE ac_atan2, real_part, imag_part, Octant, Theta
+
+	MOV AL, Octant
+	MOV EBX, Theta
+
+	INVOKE io_functions
+
+
 
  INVOKE ExitProcess,0
 
